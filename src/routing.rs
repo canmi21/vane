3bcad1059a91ad07ba95b30af367366e9ffdc133
Canmi <9997200@qq.com>
2025-09-10 13:15:32 +0800

    chore: remove outdated comment in routing.rs

--- src/routing.rs
+++ src/routing.rs
@@ -5,7 +5,6 @@ use crate::state::AppState;
 use std::sync::Arc;
 
 pub fn find_target_url(host: &str, path: &str, state: &Arc<AppState>) -> Option<String> {
-    // The lookup is now simpler as `domains` directly holds DomainConfig.
     let domain_config = state.config.domains.get(host)?;
 
     let route: &Route = domain_config
