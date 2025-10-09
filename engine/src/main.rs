76cb933b1b4c39b2499bb544eaf0c5cc1ebd76bc
Canmi <9997200@qq.com>
2025-10-09 20:51:13 +0800

    feat: add engine main module

---
+++ engine/src/main.rs
@@ -0,0 +1,28 @@
+/* engine/src/main.rs */
+
+use std::env;
+
+mod bootstrap;
+mod common;
+mod daemon;
+mod middleware;
+
+#[tokio::main]
+async fn main() {
+	// Handle command-line version argument.
+	if let Some(arg) = env::args().nth(1) {
+		if arg == "version" || arg == "--version" {
+			println!(
+				"{} {} ({} {})",
+				env!("CARGO_PKG_NAME"),
+				env!("CARGO_PKG_VERSION"),
+				env!("GIT_COMMIT_SHORT"),
+				env!("BUILD_DATE")
+			);
+			return; // Exit after printing version.
+		}
+	}
+
+	// If no version arg, start the server.
+	bootstrap::start().await;
+}
