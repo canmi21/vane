9432e590bb1d3d34ce3ff8536aa1e56ef56083be
Canmi <9997200@qq.com>
2025-10-10 15:16:42 +0800

    feat: add tailwindcss to vite config

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
 			autoCodeSplitting: true,
 		}),
 		react(),
+		tailwindcss(),
 	],
 	resolve: {
 		alias: {
