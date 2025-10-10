a820084746ae67073e5a7290954c11296db8c5a6
Canmi <9997200@qq.com>
2025-10-10 15:55:38 +0800

    feat: add type definitions for Vite and vite-plugin-svgr

---
+++ console/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/* src/vite-env.d.ts */
+
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module "*.svg?react" {
+	import * as React from "react";
+	const ReactComponent: React.FunctionComponent<
+		React.ComponentProps<"svg"> & { title?: string }
+	>;
+	export default ReactComponent;
+}
