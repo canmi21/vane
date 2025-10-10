838f729638d14de2b44a3aabe3936d8bd96d4982
Canmi <9997200@qq.com>
2025-10-10 16:37:12 +0800

    refactor: remove Framer Motion wrappers from RootLayout

--- console/src/routes/__root.tsx
+++ console/src/routes/__root.tsx
@@ -4,15 +4,19 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Sidebar } from "~/components/sidebar/sidebar";
 
-const RootLayout = () => (
-	<div className="flex h-dvh">
-		<Sidebar />
-		<main className="flex-1 overflow-y-auto p-8 bg-[var(--color-bg-alt)]">
-			<Outlet />
-		</main>
+// Note: Framer Motion imports have been removed.
 
-		<TanStackRouterDevtools />
-	</div>
-);
+const RootLayout = () => {
+	return (
+		<div className="flex h-dvh">
+			<Sidebar />
+			<main className="flex-1 overflow-y-auto p-8 bg-[var(--color-bg-alt)]">
+				{/* The animation wrappers are gone, leaving a direct Outlet. */}
+				<Outlet />
+			</main>
+			<TanStackRouterDevtools />
+		</div>
+	);
+};
 
 export const Route = createRootRoute({ component: RootLayout });
