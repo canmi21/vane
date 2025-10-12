75edc84af8af91d987d2ed6201b750c42578ff55
Canmi <9997200@qq.com>
2025-10-12 23:50:05 +0800

    feat: add instance-specific Cors route

---
+++ console/src/routes/$instance/cors/index.tsx
@@ -0,0 +1,15 @@
+/* src/routes/$instance/cors/index.tsx */
+
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/$instance/cors/")({
+	component: CorsPage,
+});
+
+function CorsPage() {
+	return (
+		<div>
+			<h3 className="text-2xl font-bold">CORS</h3>
+		</div>
+	);
+}
