7b11a4267d81a8526efc38934e332ceb7ab8ab14
Canmi <9997200@qq.com>
2025-10-12 23:50:04 +0800

    feat: redirect to a new instance-specific homepage

--- console/src/routes/index.tsx
+++ console/src/routes/index.tsx
@@ -1,11 +1,31 @@
 /* src/routes/index.tsx */
 
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+// A simple utility to generate a random string.
+const generateRandomId = () => Math.random().toString(36).substring(2, 7);
+
+const LOCAL_STORAGE_KEY = "@vane/default-instance";
 
 export const Route = createFileRoute("/")({
-	component: Index,
-});
+	// This function runs before the component loads.
+	beforeLoad: () => {
+		// Try to get the default instance from localStorage.
+		let instance = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-function Index() {
-	return null;
-}
+		if (!instance) {
+			// If it doesn't exist, generate a new one.
+			instance = generateRandomId();
+			// Store it for future visits.
+			localStorage.setItem(LOCAL_STORAGE_KEY, instance);
+		}
+
+		// Redirect to the instance-specific homepage.
+		throw redirect({
+			to: "/$instance",
+			params: { instance },
+		});
+	},
+	// This component will never be rendered due to the redirect.
+	component: () => null,
+});
