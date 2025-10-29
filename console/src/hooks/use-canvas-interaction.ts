7cd520929f76ab26c7c528241689523e70b39141
Canmi <9997200@qq.com>
2025-10-29 10:34:23 +0800

    chore: format use-canvas-interaction hook

--- console/src/hooks/use-canvas-interaction.ts
+++ console/src/hooks/use-canvas-interaction.ts
@@ -16,7 +16,7 @@ export type InteractionMode =
 			fromNodeId: string;
 			fromHandle: string;
 			fromPosition: { x: number; y: number };
-	};
+	  };
 
 interface UseCanvasInteractionProps {
 	scale: number;
