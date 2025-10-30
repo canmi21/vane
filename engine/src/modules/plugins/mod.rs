076cc524b120c173e960ce30ab97fcd93bd2a5a3
Canmi <9997200@qq.com>
2025-10-30 16:33:03 +0800

    feat: add plugin handler and manager modules

--- engine/src/modules/plugins/mod.rs
+++ engine/src/modules/plugins/mod.rs
@@ -1,3 +1,8 @@
 /* engine/src/modules/plugins/mod.rs */
 
 pub mod builtin;
+pub mod handler;
+pub mod manager;
+
+// Publicly export the primary data structures for use in other parts of the application.
+pub use manager::{Plugin, PluginSource};
