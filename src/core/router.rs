1fb7d68a9d6c806ab1a1bfaae0e0a0b03daeab16
Canmi <9997200@qq.com>
2025-11-07 23:34:09 +0800

    feat: create router with root and fallback handlers

--- src/core/router.rs
+++ src/core/router.rs
@@ -1,2 +1,14 @@
 /* src/core/router.rs */
 
+use crate::{core::response, core::root::root_handler};
+use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+
+pub fn create_router() -> Router {
+	Router::new()
+		.route("/", get(root_handler))
+		.fallback(not_found_handler)
+}
+
+async fn not_found_handler() -> impl IntoResponse {
+	response::error(StatusCode::NOT_FOUND, "Resource not found.".to_string())
+}
