124dee1d80b355ef6212a7256a425661ec0289da
Canmi <9997200@qq.com>
2025-12-23 21:38:15 +0800

    feat: introduce L7 processing step builders for integration tests

---
+++ integration/pkg/config/advanced/l7.go
@@ -0,0 +1,52 @@
+/* integration/pkg/config/advanced/l7.go */
+package advanced
+
+// NewFetchUpstream creates a ProcessingStep for internal.driver.upstream.
+// It acts as an L7 Middleware/Driver.
+// Inputs:
+// - url_prefix: "http://127.0.0.1:8080"
+// - version: "auto", "h1", "h2", "h3"
+// Branches:
+// - onSuccess: Executed when upstream returns a response (usually NewSendResponse).
+// - onFailure: Executed when upstream fails (usually NewAbortConnection).
+func NewFetchUpstream(urlPrefix string, version string, onSuccess ProcessingStep, onFailure ProcessingStep) ProcessingStep {
+	return ProcessingStep{
+		"internal.driver.upstream": PluginInstance{
+			Input: map[string]interface{}{
+				"url_prefix": urlPrefix,
+				"version":    version,
+			},
+			Output: map[string]ProcessingStep{
+				"success": onSuccess,
+				"failure": onFailure,
+			},
+		},
+	}
+}
+
+// NewSendResponse creates a terminator that flushes the current Container response to the client.
+// Plugin: internal.terminator.response
+// Use this after FetchUpstream to actually send the data back.
+func NewSendResponse() ProcessingStep {
+	return ProcessingStep{
+		"internal.terminator.response": PluginInstance{
+			Input:  map[string]interface{}{}, // No inputs needed, consumes Container
+			Output: nil,                      // Terminators do not have output branches
+		},
+	}
+}
+
+// NewL7StaticResponse creates a simple fixed response.
+// Plugin: internal.terminator.response
+// Useful for mocking or error pages without upstream.
+func NewL7StaticResponse(status int, body string) ProcessingStep {
+	return ProcessingStep{
+		"internal.terminator.response": PluginInstance{
+			Input: map[string]interface{}{
+				"status": status,
+				"body":   body,
+			},
+			Output: nil,
+		},
+	}
+}
