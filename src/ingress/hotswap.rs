99d281ea5653b93d7070df72289064373510a2c0
Canmi <9997200@qq.com>
2026-01-29 23:36:05 +0800

    chore: log explicit config change signals during hotswap

--- src/ingress/hotswap.rs
+++ src/ingress/hotswap.rs
@@ -10,16 +10,26 @@ use live::holder::HoldEvent;
 pub async fn start_listener_event_loop(config: &ConfigManager) {
 	let mut tcp_rx = config.listeners.tcp.subscribe();
 	let mut udp_rx = config.listeners.udp.subscribe();
+	let mut res_rx = config.resolvers.subscribe();
+	let mut app_rx = config.applications.subscribe();
 
 	log(LogLevel::Debug, "⚙ Starting listener event loop...");
 
 	loop {
 		tokio::select! {
 				Ok(event) = tcp_rx.recv() => {
+						log(LogLevel::Info, "➜ Config change signal received for TCP Listener, reloading...");
 						handle_tcp_event(event).await;
 				}
 				Ok(event) = udp_rx.recv() => {
+						log(LogLevel::Info, "➜ Config change signal received for UDP Listener, reloading...");
 						handle_udp_event(event).await;
+				}
+				Ok(_) = res_rx.recv() => {
+						log(LogLevel::Info, "➜ Config change signal received for Resolver, reloading...");
+				}
+				Ok(_) = app_rx.recv() => {
+						log(LogLevel::Info, "➜ Config change signal received for Application, reloading...");
 				}
 		}
 	}
